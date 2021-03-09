b3ce8321ba1afbd0ee6d18fd8fac97fb1c0a1487
cuong0209992 <sandtimezone92@gmail.com>
2021-03-10 00:37:55 +0700

    câu lệnh điều kiện đơn giản

---
+++ ez-if.js
@@ -0,0 +1,9 @@
+var answer = prompt("Trả lời câu hỏi“What is the “official” name of JavaScript?”")
+
+if(answer == "ECMAScript"){
+    alert("Right!");
+}
+else
+{
+    alert('Didn not know? ECMAScript!');
+}
